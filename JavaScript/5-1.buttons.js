b187752ef3f37abf680d2bfd09642cd7788ad844
Pracrobo <cpu180621@gmail.com>
2025-04-06 18:03:18 +0900

    음수불가능 숙제

--- JavaScript/5-1.buttons.js
+++ JavaScript/5-1.buttons.js
@@ -1,18 +1,20 @@
 function increment() {
-//    console.log("증가버튼 클릭");
     var result = document.getElementById("result")
-    console.log(result)   
-    // result.innerText = result.innerText + 1; 0111 이런식으로 나옴
     // DOM안에 있는건 글자다.
+    // 글자를 숫자로 변환하는 함수 : parseInt()  또는 Number;
+    // result.innerText = Number(result.innerText) + 1;
     result.innerText++;
-    //글자를 숫자로 변환하는 함수 : parseInt()  또는 Number;
-//    result.innerText = Number(result.innerText) + 1;
-    
+    let notice = document.getElementById("notice"); 
+   if (typeof notice.innerText == "string") {
+        notice.innerText = '';
+   }
 }
 function decrement() {
-//    console.log("감소버튼 클릭");
     var result = document.getElementById("result")
-    console.log(result)
     result.innerText = parseInt(result.innerText) -1;
-    // 음수로 안가게 하는 방법 만들기(if 비교)
+    let notice = document.getElementById("notice"); 
+    if (result.innerText < 0) {
+        notice.innerText = "음수는 불가능합니다! 숫자를 올리세요.";
+        result.innerText = 0;
+    }
 }
