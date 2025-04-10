7ff0454f6e51773c93d13d491a40e01219b20468
Pracrobo <cpu180621@gmail.com>
2025-04-10 10:09:19 +0900

    snake.js 추가

--- JavaScript/Canvas/snake.js
+++ JavaScript/Canvas/snake.js
@@ -1,6 +1,10 @@
-const GAME_SPEED = 200; // 화면 갱신 주기(ms)
+const GAME_SPEED = 300; // 화면 갱신 주기(ms)
 const BLOCK_SIZE = 20;
 let snake = {x: 0, y : 0}; //뱀의 시작 위치
+let food = {x : 100, y: 200};
+let direction = 'right'; // 초기 이동 방향
+let canvas;
+let context;
 
 window.onload = initialize;
 // DOM과 각종 필요한 여러 컴포넌트 로딩이 끝난 이루 실행
@@ -14,9 +18,25 @@ function initialize () {
     setInterval(gameLoop, GAME_SPEED);
 
 }
+function keyHandler(event) {   
+    switch(event.key) {
+        case 'ArrowUp':
+            direction = 'up';
+            break;
+        case 'ArrowDown':
+            direction = 'down';
+            break;
+        case 'ArrowLeft':
+            direction = 'left';
+            break;
+        case 'ArrowRight':
+            direction = 'right';
+            break;
+    }
+}
 
 function setupEventListener() {
-///    document.addEventListener('keydown', ...);
+    document.addEventListener('keydown', keyHandler);
 }
 
 
@@ -28,13 +48,42 @@ function gameLoop() {
 }
 //뱀의 위치를 이동한다
 function moveSnake() {
-    snake.x += BLOCK_SIZE;
-    snake.y += BLOCK_SIZE;
+    switch (direction) {
+        case'up':
+            snake.y -= BLOCK_SIZE;
+            break;
+        
+        case 'down':
+            snake.y += BLOCK_SIZE;
+            break;
+        
+        case 'left':
+            snake.x -= BLOCK_SIZE;
+            break;
+        
+        case 'right':
+            snake.x += BLOCK_SIZE;
+            break;
+    }
+    // snake.x += BLOCK_SIZE;
+    // snake.y += BLOCK_SIZE;
     //화면 벗어나지 않게 오른 쪽 끝 ->왼쪽 끝으로 나오기(nice versa)
     //                              -> 위로 
 
     // 화면을 벗어나지 않는 랜덤 위치에 빨간 사과(먹이)를 먹는다.
+    if (snake.x > canvas.width) {
+        snake.x = 0;
+    }
+    if( snake.x < 0) {
+        snake.x = canvas.width;
+    }
 
+    if (snake.y > canvas.height) {
+        snake.y = 0;
+    }
+    if( snake.y < 0) {
+        snake.y = canvas.height;
+    }
     // 숙제 (옵셔널)
     // 사과 먹고, 빨란 박스(사과)와 나의 머리 위치가 같으면 다시 2번을 재실행(랜덤 위치에 먹이 배치)
 
@@ -43,9 +92,15 @@ function moveSnake() {
     // 나의 몸의 길이를 배열에 넣기
 }
 
-//화면에 뱀을 그린다.
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
+    //화면에 뱀을 그린다.
     context.fillStyle = 'green';
     context.fillRect(snake.x, snake.y, BLOCK_SIZE, BLOCK_SIZE);
+    // 사과 그리기
+    if(snake.x === food.x && snake.y === food.y) {
+        food.x = Math.floor(Math.random() * canvas.width / BLOCK_SIZE) * BLOCK_SIZE;
+    }
+    context.fillStyle = 'red';
+    context.fillRect(food.x, food.y, BLOCK_SIZE, BLOCK_SIZE);
 }
--- JavaScript/JavaScript2.md
+++ JavaScript/JavaScript2.md
@@ -61,3 +61,41 @@ try ~ except
 - document.querySelector(...)의 결과는 **단일 요소 (Element)**인데,
 - 여기에 .forEach()를 쓰려고 해서 **"이건 함수 아냐!"**라고 에러를 낸 거예요.
 - 단일 요소가 아닌 querySelectorAll()을 써야한다.
+
+
+## 콜백함수(callback)
+함수의 인자로 함수를 받아서 특정 시점이 되었을때 호출되도록 사용
+
+## 함수 형태
+```plain text
+1. function name(arg) {
+}
+
+2. anonymous function (arg) (
+))'
+보통 콜백함수에서 사용함, 보통 한곳에서만 쓰일거라...
+
+3. arrow function (arg) => {
+}
+
+3-1. {}괄호가 없는 케이스 바로 한줄이 return
+3-2. {}괄호 안에 함수본문이 있는 경우 본문이 리턴되지 않는다. 필요시 return 이런식으로 반환하기
+```
+
+객체 (Object)
+많은 정보를 하나의 변수로 효율적으로 전달
+객체 안에는 속성(attribute), 함수 있음
+- 메서드(Java)
+- 함수(fuction)
+
+JS > event.keyCode로 아스키코드를 옛날에 썼다.
+
+### [암기 목록]
+2진수 0-10 암기
+16진수 ..암기
+아스키 코드 
+정규표현식
+프로토콜
+
+
+현재는 event.key로 쓴다.
