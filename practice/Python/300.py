0ec9f7504b6699da2a3cd4c51fd62aa37e6c24e6
Pracrobo <cpu180621@gmail.com>
2025-04-18 17:16:21 +0900

    300제 (15번까지)

---
+++ practice/Python/300.py
@@ -0,0 +1,43 @@
+# 300제 ~ 100
+print("Hello World")
+
+print("Mary's cosmetics")
+
+print('신씨가 소리질렀다. "도둑이야"')
+
+#6
+#오늘은 일요일
+
+print('naver;kakao;sk;sansung')
+print("naver","kakao", "sk", "sansung", sep=";")
+
+
+print("naver", "kakao", "sk", "samsung", sep="/")
+
+print("first", end="");print("second")
+
+print(5/3)
+
+#11
+samsung_electornic = 50_000
+print(10*samsung_electornic)
+
+total=298*10**12
+present_per = 50_000
+per = 15.79
+print("시가총액:" , total)
+print("현재가:" , present_per)
+print("PER:" , per)
+
+s= "hello"
+t= "python"
+print(s,t,sep="! ")
+
+print(2+2*3) 
+
+#15
+a= 128
+print(type(a))
+a ="132"
+print(type(a))
+# str
