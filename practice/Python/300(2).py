00e97bd41dadd8372247db1f93127ab7f364dc01
Pracrobo <cpu180621@gmail.com>
2025-04-21 00:58:36 +0900

    114번까지

---
+++ practice/Python/300(2).py
@@ -0,0 +1,70 @@
+#101
+# bool
+
+#102
+print(3==5) #False
+
+
+print(3 < 5) # True
+
+x = 4
+print(1 < x < 5)
+# true
+
+print((3 == 3) and (4 != 3))
+# True
+
+#106
+#print(3 => 4)
+#SyntaxError: expression cannot contain assignment, perhaps you meant "=="?
+
+if 4 < 3:
+    print("Hello World")
+# Nothing
+
+if 4 < 3:
+    print("Hello World.")
+else:
+    print("Hi, there.")
+    # Hi, there.
+    
+if True:
+    print("1")
+    print("2")
+else:
+    print("3")
+print("4")
+
+#1 \n 2 \n 4
+
+
+#110
+if True :
+    if False:
+        print("1")
+        print("2")
+    else:
+        print("3")
+else :
+    print("4")
+print("5")
+# 3 \n 5
+
+a = input()
+print(a*2)
+
+
+#112
+user = input("숫자를 입력하세요: ")
+print(10 + int(user))
+
+
+user = input("")
+if int(user) % 2 == 0:
+    print("짝수")
+else:
+    print("홀수")
+    
+# 114
+a = input()
+print(int(a) + 10)
