3141b50572db78903e70b29cb1e5ca818fed1438
Pracrobo <cpu180621@gmail.com>
2025-04-25 17:16:29 +0900

    memo v1 delete, update

--- MiniProject/1.memopad/app.js
+++ MiniProject/1.memopad/app.js
@@ -10,19 +10,21 @@ const db = new sqlite.Database('../miniapp.db', (err) => {
 })
 
 app.use(morgan('dev'));
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
         res.sendFile(path.join(__dirname, 'public', 'memopad.html'));
 });
+//전부 다 보여주기
 app.get('/api/memo', (req, res) => {
     const query = 'SELECT * FROM memo;';
     db.all(query, [], (err, memo) => {
         if (memo) res.send(memo);
     });
 });
-
+//db에 저장된 특정하나만 보여주기
 app.get('/memo/:id', (req,res) => {
     const id = req.body;
     db.get('SELECT * FROM WHERE id=?', [id], (err, row) => {
@@ -31,14 +33,39 @@ app.get('/memo/:id', (req,res) => {
     res.send({id, title, contents});
 });
 
-app.post('/memo', (req,res) => {
-    // console.log(req);
-    db.run('INSERT INTO memo (title, contents) VALUES (?, ?)', [req.body.title, req.body.contents], (err, row) => {
-        if(err) console.log('오류');
-        if(row) res.send({'title': row.title, 'contents': row.contents});
+//하나 수정하기
+app.put('/memo/:id', (req,res) => {
+    const id = req.body;
+    db.get('SELECT * FROM WHERE id=?', [id], (err, row) => {
+        console.log(row);
     });
+    res.send({id, title, contents});
 });
 
+//하나 지우기
+app.delete('/memo/:id', (req,res) => {
+    const id = req.body;
+    db.get('DELETE * FROM WHERE id=?', [id], (err) => {
+        if(err) res.status(500).send({message : '에러'});
+        return;
+    });
+    res.send({message: '성공'});
+});
+
+
+app.post('/memo', (req,res) => {
+    console.log('req.body:', req.body);
+    const { title, contents } = req.body;
+    db.run('INSERT INTO memo (title, contents) VALUES (?, ?)', [title, contents], function(err){
+        if(err) {
+            console.log('오류');
+            res.status(500).send({ message: 'DB 오류 발생'});
+            return;
+        }
+        // this.lastID: 새로 삽입된 레코드의 id 값
+        res.send({message : 'insert 성공', title: title, contents: contents, id: this.lastID })
+    });
+});
 
 
 app.listen(port, () => {
--- MiniProject/1.memopad/public/js/memo.js
+++ MiniProject/1.memopad/public/js/memo.js
@@ -3,59 +3,94 @@ document.addEventListener('DOMContentLoaded', async() => {
     const cardlist = await response.json();
     showCard(cardlist);
     const submitBtn = document.querySelector('#save');
+    
     submitBtn.addEventListener('click', async(e) => {
+        e.preventDefault(); // form submit 방지
         const data = await submit();
         if(data) {
-            enrollCard(selectCard);
+            showCard(data);
         }
     });
 });
+
+
+    
 function showCard(data) {
-    const carddivs = document.querySelectorAll('#card-list > .col-md-4');
-    if(data && carddivs.length>0) {
+    const fragment = document.createDocumentFragment();
+    const carddivs = document.querySelector('#card-list');
+    if (Array.isArray(data)) {
         // 받은 list 값이 있다면 뿌려주기\
-        data.forEach((one, index) => {
-            if (carddivs[index]) {
-                carddivs[index].innerHTML = `
-                <div class="card-body">
-                        <h5 class="card-title"><span id="hidden">${one.id}</span>${one.title}</h5>
-                        <p class="card-text">${one.contents}</p>
-                        <button type="button" id="updateBtn" class="btn btn-warning">수정</button>
-                        <button type="button" id="deleteBtn" class="btn btn-success">삭제</button>
-                    </div>
-                `
-            }
-            document.getElementById('hidden').style.display="none";
-        });
+        Object.entries(data).forEach(([key, value]) => {
+            const cardDiv = document.createElement('div');
+            cardDiv.classList.add('col-md-3');
+            cardDiv.innerHTML = `
+            <div class="card-body">
+            <span class="hidden">${value.id}</span>
+            <h5 class="card-title"> ${value.title}</h5>
+            <p class="card-text">${value.contents}</p>
+            <button type="button" class="btn btn-warning updateBtn data-id='${value.id}'">수정</button>
+            <button type="button" class="btn btn-success deleteBtn data-id='${value.id}'">삭제</button>
+            </div>            `
+            fragment.appendChild(cardDiv);
+            deleteFuc(key, value.id);
+        });  
+        carddivs.appendChild(fragment); // 한 번에 삽입
+    }else{
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col-md-3');
+        cardDiv.innerHTML = `
+        <div class="card-body">
+        <span class="hidden"  data-id="${data.id}">${data.id}</span>
+        <h5 class="card-title">${data.title}</h5>
+        <p class="card-text">${data.contents}</p>
+        <button type="button" class="btn btn-warning updateBtn" data-id='${data.id}'">수정</button>
+        <button type="button" class="btn btn-success deleteBtn" data-id='${data.id}'">삭제</button>
+        </div>
+        `;
+        carddivs.appendChild(cardDiv);
     }
+    // span 숨기기
+    const spans = document.querySelectorAll('.hidden');
+    spans.forEach(span => span.style.display = 'none');
+    const cardOnes = document.querySelectorAll('.card-body');
+    cardOnes.forEach(cardOne => cardOne.style.margin = '10px');
+}
+
+
+function updateFuc(idx, id) {
+    // console.log(idx, id);
+        
+}
+
+function deleteFuc(idx, id) {
+    console.log(idx, id);
+    for(let i = 0; i < buttons.length; i++) {
+        const buttons = document.getElementsByClassName('deleteBtn').item(i);
+        console.log(buttons);
+    }
+    //.forEach((idx) => {
+    //    console.log(idx);
+
+//    });
+    // .addEventListener('click', () => {
+    //     console.log('성공');
+    // });
+    // buttons.addEventListener('click', async() => {
+    //     console.log('여', id);
+    // });
+        
 }
 
 async function submit() {
     const inputTitle = document.querySelector('#input-title.form-control');
-    const title = inputTitle.value;
     const inputContents = document.querySelector('#input-text.form-control');
+    const title = inputTitle.value;
     const contents = inputContents.value;
-    
-    console.log(title, contents);
-    
     const response = await fetch('/memo', {
         method: "POST",
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({'title': title, 'contents': contents})
+        body: JSON.stringify({title: title , contents: contents})
     });
-    const data = await response.json();
+    const data = response.json();
     return data;
 }
-
-function enrollCard(contents) {
-    const cards = document.querySelectorAll('#card-list.col-md-4');
-    // cards.innerHTML(`
-    // <div class="card-body">
-    //     <h5 class="card-title"><span id="hidden">${contents.id}</span>${contents.title}</h5>
-    //     <p class="card-text">${contents.contents}</p>
-    //     <button type="button" id="updateBtn" class="btn btn-warning">수정</button>
-    //     <button type="button" id="deleteBtn" class="btn btn-success">삭제</button>
-    // </div>
-    //`);
-}
-
